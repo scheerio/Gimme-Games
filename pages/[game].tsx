164271cb48798a138d25e704c9b88c8dd89aa4d5
Ben Scheer <benjack321@gmail.com>
2022-02-17 17:36:13 -0500

    More ui for wordpunch

--- pages/[game].tsx
+++ pages/[game].tsx
@@ -7,46 +7,51 @@ type Props = {}
 function Game({}: Props) {
 
   const [modalOpen, setModalOpen] = useState(true);
+  const [triesLeft, setTriesLeft] = useState(3);
 
   const handleExitGame = () => {
     setModalOpen(!modalOpen);
+  }
+
+  const handleSubtractTries = () => {
+    setTriesLeft(prev=>prev-1);
   }
 
   return (
     <>
       {modalOpen ? <GameModal handleExitGame={handleExitGame}/> : null}
       <Layout>
-          {/* <div h-screen bg-black text-green-500>
-            <h1>hello</h1>
-          </div> */}
         <div className="h-fit text-white p-10 bg-black overflow-x-hidden flex flex-col justify-center items-center">
           <h1 className="text-3xl">Wordpunch</h1>
-          <p className="p-5 text-green-500">Good luck guessing the word</p>
+          <p className="p-3 text-green-500 text-xl">Good luck guessing this word</p>
           {/* <p className="p-5 text-green-500 flex justify-center items-center">Today's hint:</p> */}
-          <ol className="p-3 flex flex-col justify-center items-center">
+          <ol className="mt-1 mb-3 flex flex-col justify-center items-center text-2xl">
             <div className="m-1 p-1 bg-white rounded w-fit">
-              <li className="text-black">F_ight</li>
+              <li className="text-black">F<span className="text-green-500 font-semibold">_</span>ight</li>
             </div>
             <div className="m-1 p-1 bg-white rounded w-fit">
-              <li className="text-black">F_ight</li>
+              <li className="text-black">F<span className="text-green-500 font-semibold">_</span>ight</li>
             </div>
             <div className="m-1 p-1 bg-white rounded w-fit">
-              <li className="text-black">F_ight</li>
+              <li className="text-black">F<span className="text-green-500 font-semibold">_</span>ight</li>
             </div>
             <div className="m-1 p-1 bg-white rounded w-fit">
-              <li className="text-black">F_ight</li>
+              <li className="text-black">F<span className="text-green-500 font-semibold">_</span>ight</li>
             </div>
             <div className="m-1 p-1 bg-white rounded w-fit">
-              <li className="text-black">F_ight</li>
+              <li className="text-black">F<span className="text-green-500 font-semibold">_</span>ight</li>
             </div>
           </ol>
           <div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="mt-3 flex flex-col justify-center items-center">
               <div className="flex flex-row">
-                <input autoFocus className="w-40 text-center p-3 bg-gray-600 rounded text-xl"></input>
-                <button className="ml-3 p-5 transform duration-300 content-center bg-transparent hover:bg-white bg-green-500 font-semibold hover:text-black border border-green-100 hover:border-transparent rounded">Enter</button>
+                <input autoFocus className="w-40 text-center outline-0 p-3 bg-gray-600 rounded text-4xl"></input>
+                <button className="text-xl ml-3 p-5 transform duration-300 content-center bg-transparent hover:bg-white bg-green-500 font-semibold hover:text-black border border-green-100 hover:border-transparent rounded" onClick={handleSubtractTries}>Enter</button>
               </div>
-              <p className="p-3 text-green-500">Tries left: 3</p>
+              <div className="mt-4 flex flex-row text-xl">
+                <p className="text-green-500">Tries left:</p>
+                <p className="text-white ml-2">{triesLeft}</p>
+              </div>
             </div>
           </div>
         </div>
