7fa6bf178f49ffa4ecfe289b1327799996206c5e
Ben Scheer <benjack321@gmail.com>
2022-02-25 16:20:08 -0500

    Adding google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Component {...pageProps} />
-  )
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string | undefined) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+
+  return <Component {...pageProps} />
 }
 
 export default MyApp
